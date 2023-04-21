4eb9ef729a66307a4b6ca1f17b4c063fa02699a3
Drazen Drinic <drazendrinic@hotmail.com>
2023-04-21 17:06:20 +0200

    add: skeleton prior dev

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const menuArray = [
+export const foodMenu = [
     {
         name: "Pizza",
         ingredients: ["pepperoni", "mushrom", "mozarella"],
@@ -14,10 +14,62 @@ const menuArray = [
         id: 1
     },
         {
+        name: "Burito",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        emoji: "🌯",
+        id: 2
+    },
+    {
+        name: "Taco",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🌮",
+        id: 1
+    },
+    {
+        name: "Hot Dog",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🌭",
+        id: 1
+    },
+]
+
+export const drinkMenu = [
+    {
+        name: "Bubble Tea",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        id: 0,
+        price: 14,
+        emoji: "🧋"
+    },
+    {
+        name: "Coffee",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "☕",
+        id: 1
+    },
+        {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
         emoji: "🍺",
         id: 2
-    }
+    },
+    {
+        name: "Coctail",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍹",
+        id: 1
+    },
+    {
+        name: "Soft Drink",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🥤",
+        id: 1
+    },
 ]
